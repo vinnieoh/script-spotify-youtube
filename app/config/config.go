61cb4b8e8571d75e918f83ea73d4ce806c3a27a6
Vinicius de Oliveira <viniciusoliveira926@gmail.com>
2025-03-27 23:36:40 -0300

    feat: add config dotenv projeto

--- app/config/config.go
+++ app/config/config.go
@@ -1,1 +1,31 @@
 package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	SPOTIFY_CLIENT_ID string
+	SPOTIFY_CLIENT_SECRET string
+	YOUTUBE_API_KEY string
+}
+
+
+func NewConfig() *Config {
+
+	if err := godotenv.Load("./dotenv_files/.env"); err != nil {
+        log.Printf("Error loading .env file")
+    }
+
+	cfg := &Config{}
+	
+	cfg.SPOTIFY_CLIENT_ID = os.Getenv("SPOTIFY_CLIENT_ID")
+	cfg.SPOTIFY_CLIENT_SECRET = os.Getenv("SPOTIFY_CLIENT_SECRET")
+	cfg.YOUTUBE_API_KEY = os.Getenv("YOUTUBE_API_KEY")
+
+	return cfg
+
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/vinnieoh/script-spotify-youtube
 
 go 1.23.1
+
+require github.com/joho/godotenv v1.5.1 // indirect
